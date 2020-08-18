ab196190aa9d49a96c5f0f4d89988ee806963b93
manikrishna <smkrishna007@gmail.com>
2020-08-19 01:58:46 +0530

    Weather with coords

--- weatherreport/src/app/app.component.ts
+++ weatherreport/src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component,NgModule } from '@angular/core';
-import {WeatherService} from './weather.service'
-import{Weather} from './appdata'
+import { Component, NgModule } from '@angular/core';
+import { WeatherService } from './weather.service'
+import { Weather } from './appdata'
 
 @Component({
   selector: 'app-root',
@@ -9,20 +9,25 @@ import{Weather} from './appdata'
 })
 export class AppComponent {
   title = 'weatherreport';
-  data = new Weather('',0,0,'');  
-  wData :any;
+  data = new Weather('', 0, 0, '');
+  wData: any;
 
   jsonData: Object = new Object;
 
-  constructor(private _weatherService:WeatherService){
+  constructor(private _weatherService: WeatherService) {
   }
 
-getWeather(){ 
-  console.log(this.data.city);
-  this.jsonData =  this._weatherService.getWeatherByCountry(this.data.city).subscribe(data => this.jsonData= data);
-  //this.wData = this.json;
-  console.log(this.jsonData);
-}
-
+  getWeather() {
+    console.log(this.data.city);
+    console.log(this.data.latitude);
+    console.log(this.data.longitude);
+    if (this.data.city.length > 0) {
+      this.jsonData = this._weatherService.getWeatherByCountry(this.data.city).subscribe(data => this.jsonData = data);
+    }
+    else {
+      this.jsonData = this._weatherService.getWeatherByCoords(this.data.latitude, this.data.longitude).subscribe(data => this.jsonData = data);
+    }
+    console.log(this.jsonData);
+  }
 
 }
--- weatherreport/src/app/weather.service.ts
+++ weatherreport/src/app/weather.service.ts
@@ -22,11 +22,13 @@ private url : string
       // (data) => this.data = data); 
    }
 
-   getWeatherByZipCode(){
+   getWeatherByZipCode(lat:number,lon:number){
+    
+  }
 
-   }
-
-   getWeatherByCoord(){
-
+   getWeatherByCoords(lat:number,lon:number){
+    this.url="http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=d96a33eb4f17f26c1e1e39e491b0cb4a";
+    console.log(this.url);
+    return this.http.get(this.url); 
    }
 }
